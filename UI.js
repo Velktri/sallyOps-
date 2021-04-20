dfa74eb2241a0222d9c1c384cc2a0c64dcd97886
Velktri <murrayg226@gmail.com>
2021-04-20 00:39:28 -0700

    message passing

--- UI.js
+++ UI.js
@@ -1,16 +1,28 @@
-function testLog(tabs)
+function getTableData(tabs)
 {
+    let data = []
     tabs.forEach(tab => {
         console.log(tab)
+        browser.tabs.sendMessage(tab.id, { command: "SO_getTableData" }).then(response => {
+            console.log(response.data)
+            if (response.data.length > 0)
+            {
+                data = response.data
+            }
+        })
     })
 
+    return data
 }
 
 document.getElementById("getTableButton").onclick = () => {
 
     // get the tab for station command
-    let queries = browser.tabs.query({ url: "*://*.mozilla.org/*" })
-    queries.then(testLog)
+    let queries = browser.tabs.query({ url: "https://velktri.github.io/sallyOps-/testing/test5.html" /*"*://*.mozilla.org/*"*/ })
+    let tableData = queries.then(getTableData)
+
+    // store data
+    console.log(tableData)
     /*queries.then.forEach(query => {
         console.log(query)
         testLog()
--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function readTable()
     let data = []
     for (i = 2; i < rows.length; i++) 
     {
-        data.push(extractData(rows[i]))
+        data.push(extractRowData(rows[i]))
     }
 
     //sort data
@@ -20,11 +20,18 @@ function extractRowData(rowData)
     let children = rowData.children
 
     let cart = children[0].children[0].innerHTML
-    let route = children[1].children[0].innerHTML
-    let loc = children[4].children[0].innerHTML
-    let status = children[5].children[0].innerHTML
+    let route = children[1].children[0].textContent
+    let loc = children[3].children[0].innerHTML
+    /*let status = children[4].children[0].textContent.trim()
+    status = status.replace(/(\r\n|\n|\r)/gm, "")*/
 
-    return { cart, route, loc, status }
+    return { cart, route, loc /*, status */ }
 }
 
-console.log('hi from Content script')
+browser.runtime.onMessage.addListener((message) => {
+    if (message.command === "SO_getTableData")
+    {
+        let data = readTable()
+        return Promise.resolve({ data })
+    }
+})
